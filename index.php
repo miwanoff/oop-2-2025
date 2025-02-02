f34b99d9c11c4d86613051b09dc5980d79280571
miwanoff <marina.iva.noffa@gmail.com>
2025-02-02 12:39:26 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+
+class Cat
+{
+    public $age;
+
+    public function __construct($age)
+    {
+        $this->age = $age;
+    }
+
+    public function add_age()
+    {
+        $this->age++;
+    }
+}
+
+// $kitty = new Cat(5);
+// echo $kitty->age;
+
+// оголошуємо успадкований клас
+class MyCat extends Cat
+{
+    public $name;
+    function __construct($age, $name="") {
+		// викликаємо метод-конструктор батьківського класу
+		parent::__construct ($age);
+
+		// ініціалізуємо властивість, визначену у підкласі
+		$this->name = $name;
+	}
+    // визначаємо власний метод підкласу
+    public function sleep()
+    {
+        echo "\nZzzzz...";
+        $this->eat();
+    }
+
+    protected function eat(){
+        echo "\nChrum-chrum";
+    }
+}
+
+class MyCatMeow extends MyCat
+{
+    // визначаємо власний метод підкласу
+    public function meow()
+    {
+        echo "\nmeow!";
+    }
+
+    public function sleep()
+    {
+        echo "\nZz...";
+    }
+}
+
+$kitty = new MyCat(5);
+echo $kitty->age;
+$kitty->add_age();
+echo $kitty->age;
+
+$kitty->sleep();
+// $kitty->eat();
+
+// $kitty1 = new MyCatMeow(5);
+// echo $kitty1->age;
+// $kitty1->add_age();
+// echo $kitty1->age;
+
+// $kitty1->meow();
+// $kitty1->sleep();

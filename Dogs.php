b87642d0d6e1263d2bc3cd51ca9440e5ffdc36e7
miwanoff <marina.iva.noffa@gmail.com>
2025-02-08 13:38:16 +0200

    Add interface

--- Dogs.php
+++ Dogs.php
@@ -1,46 +1,64 @@
 <?php
-
-abstract class Animal {
-	private $name;
-
-	public function __construct($name) {
-		$this->name = $name;
-	}
-
-	abstract public function say();
-
-	public function getName() {
-		return $this->name;
-	}
+interface CanSay
+{
+    public function say();
 }
 
-class Cat extends Animal {
-
-	public function __construct($name) {
-		parent::__construct($name);
-	}
-
-	public function say() {
-		echo "meow-meow";
-	}
+interface CanSay1
+{
+    public function say();
 }
 
-class Dog extends Animal {
+abstract class Animal implements CanSay, CanSay1
+{
+    private $name;
 
-	public function __construct($name) {
-		parent::__construct($name);
-	}
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
 
-	public function say() {
-		echo "woof-woof!";
-	}
+    // abstract public function say();
+
+    public function getName()
+    {
+        return $this->name;
+    }
 }
 
-$arr= array(new Dog("Jack"), new Dog("Box"), new Cat("Lusi"), new Cat("Mina"));
+class Cat extends Animal
+{
+
+    public function __construct($name)
+    {
+        parent::__construct($name);
+    }
+
+    public function say()
+    {
+        echo "meow-meow";
+    }
+}
+
+class Dog extends Animal
+{
+
+    public function __construct($name)
+    {
+        parent::__construct($name);
+    }
+
+    public function say()
+    {
+        echo "woof-woof!";
+    }
+}
+
+$arr = [new Dog("Jack"), new Dog("Box"), new Cat("Lusi"), new Cat("Mina")];
 
 foreach ($arr as $value) {
-	if ($value instanceof Animal){
-		print($value->getName()." say:\n");
-		print($value->say()."\n");
-	}
+    if ($value instanceof Animal) {
+        print($value->getName() . " say:\n");
+        print($value->say() . "\n");
+    }
 }

119358c2e34787271ca9b9df62f87c48518c0e59
miwanoff <marina.iva.noffa@gmail.com>
2025-02-09 13:05:47 +0200

    Add error_log

--- error.php
+++ error.php
@@ -7,6 +7,8 @@
 
 function myErrorHandler($errno, $msg, $file, $line)
 {
+   // error_log("Помилка $errno, файл $file, рядок $line: $msg");
+   error_log("Помилка $errno, файл $file, рядок $line: $msg", 3 , "tmp/my-errors.log");
     echo "Произошла ошибка с кодом $errno\n";
     echo "Файл: $file, строка $line.\n";
     echo "Текст ошибки: $msg";
